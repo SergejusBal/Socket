c541cc1b12c268d8c073be3ab2d1abee3eee8934
Sergejus <Sergejus.balciunas@gmail.com>
2024-09-19 18:04:54 +0300

    AA

--- src/main/java/WebSocket/Socket/ChatRoom/ChatController.java
+++ src/main/java/WebSocket/Socket/ChatRoom/ChatController.java
@@ -1,17 +1,26 @@
 package WebSocket.Socket.ChatRoom;
 
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 public class ChatController {
 
+    private final SimpMessagingTemplate messagingTemplate;
+    public ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
-    private List<String> namelist = new ArrayList<>();
+
+    private HashMap<String, String> namelist = new HashMap<>();
 
     @MessageMapping("/chat")
     @SendTo("/topic/chat")
@@ -20,12 +29,20 @@ public class ChatController {
     }
     @MessageMapping("/name")
     @SendTo("/topic/name")
-    public List<String> setName(Name name) throws Exception {
-        if(name.getName().isEmpty()) namelist.add("Anonymous");
-        else if(namelist.contains(name.getName())) return namelist;
-        else namelist.add(name.getName());
-        return namelist;
+    public List<String> setName(Name name, @Header("simpSessionId") String sessionId) throws Exception {
+
+        if(namelist.isEmpty()) namelist.put("","Anonymous");
+
+        if(namelist.containsValue(name.getName())) new ArrayList<>(namelist.values());
+        else namelist.put(sessionId, name.getName());
+
+        return new ArrayList<>(namelist.values());
     }
+
+    public void removeUser(String sessionId)  {
+        if(namelist.remove(sessionId) != null) messagingTemplate.convertAndSend("/topic/name", new ArrayList<>(namelist.values()));
+    }
+
 
 }
 
---
+++ src/main/java/WebSocket/Socket/WebSocketDisconnectListener.java
@@ -0,0 +1,25 @@
+package WebSocket.Socket;
+import WebSocket.Socket.ChatRoom.ChatController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+public class WebSocketDisconnectListener {
+
+    @Autowired
+    private ChatController chatController;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String sessionId = headerAccessor.getSessionId();
+
+        chatController.removeUser(sessionId);
+
+    }
+}
